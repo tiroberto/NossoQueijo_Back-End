92c2a81f993f25dd4c2a0e150063440bc3dc201b
Humberto Júnior <Humberto Jnior@DESKTOP-HUMBERT>
2021-10-13 00:46:08 -0300

    .

--- NossoQueijo.WebAPI/Startup.cs
+++ NossoQueijo.WebAPI/Startup.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using NossoQueijo.Aplicacao;
+using NossoQueijo.Dominio.Interfaces.Aplicacao;
+using NossoQueijo.Dominio.Interfaces.Repositorio;
+using NossoQueijo.Repositorio.RepositoriosEF;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NossoQueijo.WebAPI
 {
     public class Startup
     {
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,7 +25,87 @@ namespace NossoQueijo.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Cidade
+            services.AddTransient<ICidadeAplicacao, CidadeAplicacao>();
+            services.AddTransient<ICidadeRepositorio, CidadeRepositorio>();
+
+            // Endereco
+            services.AddTransient<IEnderecoAplicacao, EnderecoAplicacao>();
+            services.AddTransient<IEnderecoRepositorio, EnderecoRepositorio>();
+
+            // Estado
+            services.AddTransient<IEstadoAplicacao, EstadoAplicacao>();
+            services.AddTransient<IEstadoRepositorio, EstadoRepositorio>();
+
+            // EstoquePorData
+            services.AddTransient<IEstoquePorDataAplicacao, EstoquePorDataAplicacao>();
+            services.AddTransient<IEstoquePorDataRepositorio, EstoquePorDataRepositorio>();
+
+            // FichaProducao
+            services.AddTransient<IFichaProducaoAplicacao, FichaProducaoAplicacao>();
+            services.AddTransient<IFichaProducaoRepositorio, FichaProducaoRepositorio>();
+
+            // FormaPagamento
+            services.AddTransient<IFormaPagamentoAplicacao, FormaPagamentoAplicacao>();
+            services.AddTransient<IFormaPagamentoRepositorio, FormaPagamentoRepositorio>();
+
+            // Pedido
+            services.AddTransient<IPedidoAplicacao, PedidoAplicacao>();
+            services.AddTransient<IPedidoRepositorio, PedidoRepositorio>();
+
+            // PedidoProduto
+            services.AddTransient<IPedidoProdutoAplicacao, PedidoProdutoAplicacao>();
+            services.AddTransient<IPedidoProdutoRepositorio, PedidoProdutoRepositorio>();
+
+            // Produto
+            services.AddTransient<IProdutoAplicacao, ProdutoAplicacao>();
+            services.AddTransient<IProdutoRepositorio, ProdutoRepositorio>();
+
+            // Status
+            services.AddTransient<IStatusAplicacao, StatusAplicacao>();
+            services.AddTransient<IStatusRepositorio, StatusRepositorio>();
+
+            // TipoUsuario
+            services.AddTransient<ITipoUsuarioAplicacao, TipoUsuarioAplicacao>();
+            services.AddTransient<ITipoUsuarioRepositorio, TipoUsuarioRepositorio>();
+
+            // Usuario
+            services.AddTransient<IUsuarioAplicacao, UsuarioAplicacao>();
+            services.AddTransient<IUsuarioRepositorio, UsuarioRepositorio>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:8101/",
+                                                          "http://localhost:8101",
+                                                          "https://localhost:8101/",
+                                                          "https://localhost:8101",
+                                                          "http://localhost:8100/",
+                                                          "http://localhost:8100",
+                                                          "https://localhost:8100/",
+                                                          "https://localhost:8100")
+                                      .AllowAnyHeader()
+                                      .AllowAnyMethod();
+                                  });
+            });
             services.AddControllers();
+            services.AddSwaggerGen(c => {
+
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "NossoQueijo",
+                        Version = "v1",
+                        Description = "E-commerce de queijos",
+                        Contact = new OpenApiContact
+                        {
+                            Name = "Humberto Júnior",
+                            Url = new Uri("https://github.com/tiroberto")
+                        }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,10 +117,18 @@ namespace NossoQueijo.WebAPI
             }
 
             app.UseHttpsRedirection();
+             
+            // Ativando middlewares para uso do Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "NossoQueijo");
+            });
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(MyAllowSpecificOrigins);
 
             app.UseEndpoints(endpoints =>
             {
